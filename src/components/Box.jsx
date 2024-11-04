8031be60ab1246ebc080db21558c5f99852586cf
saritadipa34 <saritadipa34@gmail.com>
2024-11-04 16:24:41 +0530

    Done with UI

---
+++ src/components/Box.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './/Box.css'
+
+const Box = () => {
+  return (
+    <div className='h-5 w-5 border-black'>
+      <h1>X</h1>
+    </div>
+  )
+}
+
+export default Box
